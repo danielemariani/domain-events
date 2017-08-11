3d133d21ae264ae36e08ddde61129b404a65687b
Daniele Mariani <mariani_daniele87@live.it>
2017-08-11 09:31:15 +0200

    Input validation on EventBus "dispatch" and "register" operations

--- src/EventBus.js
+++ src/EventBus.js
@@ -1,16 +1,24 @@
 
+const DomainEvent = require('./DomainEvent');
+
 class EventBus {
 
   constructor() {
     this.registeredEvents = {};
+    this.handlersForAnyEvent = [];
   }
 
-  register(aEventName, aEventHandler) {
+  register(aEventHandler, aEventName) {
+    validateEventName(aEventName);
+    validateHandler(aEventHandler);
+
     registerHandlerForEvent
       .call(this, aEventHandler, aEventName);
   }
 
   dispatch(aEvent) {
+    validateEvent(aEvent);
+
     return Promise.all(
       dispatchEventToRegisteredHandlers
         .call(this, aEvent)
@@ -18,14 +26,72 @@ class EventBus {
   }
 }
 
+function validateEvent(aEvent) {
+  if (!(aEvent instanceof DomainEvent)) {
+    throw new TypeError('EventBus can dispatch only instances of DomainEvent');
+  }
+}
+
+function validateEventName(aEventName) {
+  if (
+    typeof aEventName !== 'string' &&
+    typeof aEventName !== 'undefined'
+  ) {
+    throw new TypeError(`EventBus "register" method was called with an invalid event name: ${aEventName}`);
+  }
+}
+
+function validateHandler(aEventHandler) {
+  if (
+    !isAnObjectHandler(aEventHandler) &&
+    !isAFunctionHandler(aEventHandler)
+  ) {
+    throw new TypeError('EventBus "register" method was called with an invalid event handler');
+  }
+}
+
+function isAnObjectHandler(aEventHandler) {
+  return (
+    typeof aEventHandler === 'object' &&
+    typeof aEventHandler.handle === 'function'
+  );
+}
+
+function isAFunctionHandler(aEventHandler) {
+  return typeof aEventHandler === 'function';
+}
+
 function registerHandlerForEvent(aEventHandler, aEventName) {
+  if (aEventName) {
+    registerHandlerForSpecificEvent
+      .call(this, aEventHandler, aEventName);
+  } else {
+    registerHandlerForAnyEvent
+      .call(this, aEventHandler);
+  }
+}
+
+function registerHandlerForSpecificEvent(aEventHandler, aEventName) {
   listHandlersForEvent
     .call(this, aEventName)
     .push(aEventHandler);
 }
 
+function registerHandlerForAnyEvent(aEventHandler) {
+  this.handlersForAnyEvent
+    .push(aEventHandler);
+}
+
+function listHandlersForEvent(aEventName) {
+  if (!this.registeredEvents[aEventName]) {
+    this.registeredEvents[aEventName] = [];
+  }
+
+  return this.registeredEvents[aEventName];
+}
+
 function dispatchEventToRegisteredHandlers(aEvent) {
-  return listHandlersForEvent
+  return listAllHandlersForEvent
     .call(this, aEvent.name())
     .map(mapHandlerToExecution);
 
@@ -38,22 +104,25 @@ function dispatchEventToRegisteredHandlers(aEvent) {
   }
 }
 
-function listHandlersForEvent(aEventName) {
-  if (!this.registeredEvents[aEventName]) {
-    this.registeredEvents[aEventName] = [];
-  }
-
-  return this.registeredEvents[aEventName];
+function listAllHandlersForEvent(aEventName) {
+  return this.handlersForAnyEvent
+    .concat(listHandlersForEvent.call(this, aEventName));
 }
 
 function dispatchEventToHandler(aEvent, aEventHandler) {
-  aEventHandler.handle(aEvent);
+  if (isAnObjectHandler(aEventHandler)) {
+    aEventHandler.handle(aEvent);
+  }
+
+  if (isAFunctionHandler(aEventHandler)) {
+    aEventHandler(aEvent);
+  }
 }
 
 function deferredExecutionOf(aFunction, aContext, aListOfArguments) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(deferExecutionOf(
+      resolve(executeDeferred(
         aFunction,
         aContext,
         aListOfArguments
@@ -62,7 +131,7 @@ function deferredExecutionOf(aFunction, aContext, aListOfArguments) {
   });
 }
 
-function deferExecutionOf(aFunction, aContext, aListOfArguments) {
+function executeDeferred(aFunction, aContext, aListOfArguments) {
   return aFunction.apply(
     aContext,
     aListOfArguments
--- src/__test__/EventBus.spec.js
+++ src/__test__/EventBus.spec.js
@@ -6,9 +6,83 @@ const DomainEvent = require('../DomainEvent');
 
 describe('EventBus', () => {
   let eventBus;
+  let aObjectHandler;
+  let aFunctionHandler;
 
   beforeEach(() => {
+    aObjectHandler = { handle: jest.fn() };
+    aFunctionHandler = jest.fn();
+
     eventBus = new EventBus();
+  });
+
+  describe('when requested to register a handler for a Event', () => {
+    describe('should accept as event name', () => {
+      it('a "string" value', () => {
+        expect(() => {
+          eventBus.register(
+            aFunctionHandler,
+            'EVENT'
+          );
+        })
+          .not.toThrow();
+      });
+
+      it('a "undefined" value to attach the handler to any event', () => {
+        expect(() => {
+          eventBus.register(
+            aFunctionHandler
+          );
+        })
+          .not.toThrow();
+      });
+    });
+
+    describe('should accept as handler', () => {
+      it('an object with a handle method', () => {
+        expect(() => {
+          eventBus.register(
+            aObjectHandler,
+            'EVENT'
+          );
+        })
+          .not.toThrow();
+      });
+
+      it('a plain function', () => {
+        expect(() => {
+          eventBus.register(
+            aFunctionHandler,
+            'EVENT'
+          );
+        })
+          .not.toThrow();
+      });
+    });
+
+    describe('should throw a TypeError', () => {
+      it('if the event name is not omitted and is not a plain string', () => {
+        expect(() => {
+          eventBus.register(() => {}, null);
+        })
+          .toThrow(TypeError);
+      });
+
+      it('if the handler is not an object with a "handle" method', () => {
+        expect(() => {
+          eventBus.register({}, 'EVENT');
+        })
+          .toThrow(TypeError);
+      });
+
+      it('if the handler is not a function', () => {
+        expect(() => {
+          eventBus.register(null, 'EVENT');
+        })
+          .toThrow(TypeError);
+      
+      });
+    });
   });
 
   describe('when requested to dispatch an event', () => {
@@ -25,12 +99,49 @@ describe('EventBus', () => {
       };
 
       eventBus.register(
-        aEventName,
-        aEventHandler
+        aEventHandler,
+        aEventName
       );
     });
 
+    it('should throw if the event is not a valid DomainEvent instance', () => {
+      let aNotValidEvent = { name: jest.fn() };
+
+      expect(() => {
+        eventBus.dispatch(aNotValidEvent);
+      })
+        .toThrow(TypeError);
+    });
+
     describe('and a handler is registered for the event', () => {
+      describe('and the handler is an object with a "handle" method', () => {
+        it('should call the handler "handle" method with the event', () => {
+          return eventBus.dispatch(aEvent)
+            .then(onDispatchCompleted)
+            .catch(e => { throw e; });
+
+          function onDispatchCompleted() {
+            expect(aEventHandler.handle)
+              .toHaveBeenCalledWith(aEvent);
+          }
+        });
+      });
+
+      describe('and the handler is a function', () => {
+        it('should call the function with the event', () => {
+          eventBus.register(aFunctionHandler, 'EVENT');
+
+          return eventBus.dispatch(aEvent)
+            .then(onDispatchCompleted)
+            .catch(e => { throw e; });
+
+          function onDispatchCompleted() {
+            expect(aFunctionHandler)
+              .toHaveBeenCalledWith(aEvent);
+          }
+        });
+      });
+
       it('should dispatch the event to the handler asyncronously', () => {
         let dispatchOperation = eventBus
           .dispatch(aEvent);
@@ -53,13 +164,11 @@ describe('EventBus', () => {
       let anotherEventHandler;
 
       beforeEach(() => {
-        anotherEventHandler = {
-          handle: jest.fn()
-        };
+        anotherEventHandler = aFunctionHandler;
 
         eventBus.register(
-          aEventName,
-          anotherEventHandler
+          anotherEventHandler,
+          aEventName
         );
       });
 
@@ -70,7 +179,7 @@ describe('EventBus', () => {
         expect(aEventHandler.handle)
           .not.toHaveBeenCalled();
 
-        expect(anotherEventHandler.handle)
+        expect(anotherEventHandler)
           .not.toHaveBeenCalled();
 
         return dispatchOperation
@@ -81,7 +190,7 @@ describe('EventBus', () => {
           expect(aEventHandler.handle)
             .toHaveBeenCalledWith(aEvent);
 
-          expect(anotherEventHandler.handle)
+          expect(anotherEventHandler)
             .toHaveBeenCalledWith(aEvent);
         }
       });
@@ -102,6 +211,29 @@ describe('EventBus', () => {
         function onDispatchCompleted() {
           expect(aEventHandler.handle)
             .not.toHaveBeenCalled();
+        }
+      });
+    });
+
+    describe('and some handler is registered to ANY event', () => {
+      let anotherEvent;
+      let aHandlerRegisteredToAnyEvent;
+
+      beforeEach(() => {
+        anotherEvent = new DomainEvent('ANOTHER_EVENT');
+        aHandlerRegisteredToAnyEvent = jest.fn();
+
+        eventBus.register(aHandlerRegisteredToAnyEvent);
+      });
+
+      it('should dispatch any event to the handler', () => {
+        return eventBus.dispatch(aEvent)
+          .then(onDispatchCompleted)
+          .catch(e => { throw e; });
+
+        function onDispatchCompleted() {
+          expect(aHandlerRegisteredToAnyEvent)
+            .toHaveBeenCalledWith(aEvent);
         }
       });
     });
